77e0ee25dac40756d320a9dd4f76e56bee3630a4
noatakak <noatak.post@gmail.com>
2023-01-28 15:36:42 -0900

    exploring conv tensors

--- main.py
+++ main.py
@@ -6,16 +6,20 @@ from pacNet import pac_net
 
 
 def main():
-    print(torch.cuda.get_device_name())
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(f"CUDA version: {torch.version.cuda}")
+    #print(torch.cuda.get_device_name())
+    #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    #print(f"CUDA version: {torch.version.cuda}")
 
 
-    pacNet = pac_net()
-    pacNet.to(device)
+    pacNet = torch.load("savedModels/pacMODEL.pt", map_location=torch.device('cpu'))
+    #pacNet.to(device)
     print("Number of Parameters: " + str(sum(p.numel() for p in pacNet.parameters() if p.requires_grad)))
-    torch.save(pacNet, "savedModels/pacMODEL.pt");
-    temp = pacNet.parameters()
+
+    layer1Param = pacNet.layer1.weight
+    layer1bias = pacNet.layer1.bias
+    layer1Weights = torch.Tensor.detach(pacNet.layer1.weight.data).numpy()
+
+    # torch.save(pacNet, "savedModels/pacMODEL.pt");
 
     print("Hello World")
 
